83cfdd605ed89e02001f08a78f82bc6d344649ba
audriusxp <palm@inbox.lt>
2022-06-13 15:16:56 +0300

    pabaigta zuduotis

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    let x = 28;
+    let y = 77;
+    console.log(skaiciusDalinasiDvieju(28));
+    console.log(skaiciusDalinasiDvieju(77));
+
+}
+
+function skaiciusDalinasiDvieju(skaicius)
+{
+    if ((skaicius % 2 === 0)) 
+    {
+        return true;
+    }
+   
+    
+    else ((skaicius % 2 >1) ) 
+    {
+        return false;
+    }
+    
+}
